ee5939468f1540aee1c25d4907388408bc61941e
KayoMartins2405 <martinslima2424@gmail.com>
2022-09-14 22:40:18 -0300

    aula1409

---
+++ aula1509/src/atividade1/tabuada.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package atividade1;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class tabuada {
+    public static void main(String[] args) {
+        for(int i =1; i <= 10; i++){
+        
+        for (int j =1; j <=10; j++){
+            int res = i*j;
+            
+            System.out.println(i+ "x" + j + "=" + res);
+        }
+    }
+}
+}
